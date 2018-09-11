763a598b0aafb901d9effd87ef93e7ebe809c312
gaarangoa <gaarangoa@gmail.com>
2018-09-11 17:39:10 -0400

    structure project

--- src/setup.py
+++ src/setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='dnovelARG',
     version='0.1',
-    packages=find_packages(exclude=('database', 'model', 'logs', 'test')),
-    include_package_data=True,
+    packages=find_packages(
+        exclude=("model", )
+    ),
+    include_package_data=False,
     install_requires=[
         'Click',
         'BioPython',
