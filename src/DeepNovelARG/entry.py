51ca5bf16d997b8c8b685683532986a5c8834d2b
gaarangoa <gaarangoa@gmail.com>
2018-09-26 09:46:48 -0400

    fix

--- src/DeepNovelARG/entry.py
+++ src/DeepNovelARG/entry.py
@@ -2,7 +2,7 @@ import click
 from DeepNovelARG.predict import predict
 from DeepNovelARG.fasta2vec import fasta2vec
 from DeepNovelARG.train import train
-
+from DeepNovelARG.train_word_vectors import train_word_vectors
 
 @click.group()
 def cli():
@@ -16,3 +16,4 @@ def cli():
 cli.add_command(predict)
 cli.add_command(fasta2vec)
 cli.add_command(train)
+cli.add_command(train_word_vectors)
