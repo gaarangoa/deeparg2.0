cae6322603ca18e72249a4f77ff73483a4f607d6
gaarangoa <gaarangoa@gmail.com>
2018-09-26 09:45:02 -0400

    added wordvector wrapper

--- src/DeepNovelARG/train_word_vectors.py
+++ src/DeepNovelARG/train_word_vectors.py
@@ -13,7 +13,6 @@ import click
 def split_genome(genome="ATCGATATACCA", k=3):
     return re.findall('.'*k, genome)
 
-
 def genearte_genomes(genome='ATCGATATACCA', k=3, words=50):
     sentences = []
     for index in range(0, k):
@@ -27,7 +26,6 @@ def genearte_genomes(genome='ATCGATATACCA', k=3, words=50):
         sentences.append(np.array(_sentence, dtype="U"))
 
     return np.array(sentences)
-
 
 def genome_to_doc(input_file="", kmer=16, label="", f5=""):
     ''' This function transforms a sequence genome to a document of kmers '''
@@ -48,10 +46,15 @@ def genome_to_doc(input_file="", kmer=16, label="", f5=""):
 
 @click.command()
 @click.option('--inputfile', required=True, help='input fasta file')
-@click.option('--modeldir', default='', required=True, help='directory where the word2vec model was downloaded')
-@click.option('--outdir', default='', required=True, help='output directory where to store the results')
+@click.option('--outdir', default='', required=True, help='directory where the word2vec model was downloaded')
 @click.option('--kmer', default=11, help='kmer length [default: 11]')
-def train_word_vectors(inputfile, modeldir, outdir, kmer):
+@click.option('--epochs', default=100, help='number of epochs [default: 100]')
+@click.option('--dim', default=512, help='embedding dimension [default: 11]')
+@click.option('--ws', default=5, help='window size [default: 5]')
+@click.option('--thread', default=10, help='threads [default: 10]')
+@click.option('--minCount', default=5, help='minimum kmer count [default: 5]')
+
+def train_word_vectors(inputfile, outdir, kmer, epochs, dim, ws, thread, minCount):
     '''
     train word vectors using fasttext.
 
@@ -63,4 +66,42 @@ def train_word_vectors(inputfile, modeldir, outdir, kmer):
     model from fasttext to generate a word vectors representation.
 
     '''
-    pass
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    log_file = logging.FileHandler(filename=outdir + '/wordvectors.log',)
+    log_stdout = logging.StreamHandler(sys.stdout)
+    handlers = [log_file, log_stdout]
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(levelname)s %(asctime)s - %(message)s",
+        handlers=handlers
+    )
+
+    log = logging.getLogger()
+
+    log.info(f'getting sentences from {inputfile}: started')
+    x = genome_to_doc(input_file=inputfile, kmer=11)
+    log.info(f'getting sentences from {inputfile}: done')
+
+    log.info(f'storing preprocesed file to {outdir}/sentences.tsv')
+    fo = open(f'{outdir}/sentences.tsv', 'w')
+    for i in tqdm(x):
+        for j in i['sentences']:
+            fo.write(" ".join(j)+'\n')
+
+    fasttext_cmd = f'fasttext skipgram \
+        -input {outdir}/sentences.tsv \
+        -output {outdir}/model \
+        -minn 4 \
+        -maxn 9 \
+        -dim {dim} \
+        -ws {ws} \
+        -epoch {epoch} \
+        -thread {thread} \
+        -minCount {minCount} '
+    log.info('FastText: {fasttext_cmd}')
+    os.system(fasttext_cmd)
+    log.info('The process is done :)')
